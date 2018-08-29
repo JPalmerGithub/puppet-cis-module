2db448d3c2d89a42bfcbc0f7eb6af5854ee68a7d
root <root@ip-172-30-0-40.ec2.internal>
2018-08-29 07:26:34 +0000

    fix r141

--- manifests/cis_benchmarks/r141.pp
+++ manifests/cis_benchmarks/r141.pp
@@ -6,4 +6,15 @@ class cis::cis_benchmarks::r141 {
     onlyif => '/usr/bin/test -e /boot/grub/menu.lst',
   }
 
+  exec { 'grub_cfg_ownership':
+    command => '/bin/chown root:root /boot/grub2/grub.cfg',
+    onlyif => '/bin/test ! $(/bin/stat -c %U%G /boot/grub2/grub.cfg) = rootroot',
+  }
+  
+  exec { 'grub_cfg_permission':
+    command => '/bin/chmod 600 /boot/grub2/grub.cfg',
+    onlyif => '/bin/test ! $(/bin/stat -c %a /boot/grub2/grub.cfg) = 600',
+  }
+
+
 }
