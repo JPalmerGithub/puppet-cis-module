da9169f474f21fc60d7422a317ac93268431676b
root <root@ip-172-30-0-40.ec2.internal>
2018-08-29 08:25:10 +0000

    fix r2211 install ntp

--- manifests/cis_benchmarks/r2211.pp
+++ manifests/cis_benchmarks/r2211.pp
@@ -11,4 +11,27 @@ class cis::cis_benchmarks::r2211 {
     onlyif => 'chkconfig --list | grep dovecot | grep on'
   }
 
+
+  package { 'install_ntp':
+    name => "ntp",
+    ensure => "latest",
+    allow_virtual => false,
+  }
+
+  file { 'ntp_config':
+    path => "/etc/ntp.conf",
+    ensure => present,
+    source => 'puppet:///modules/cis/ntp/ntp.conf',
+    require => Package['install_ntp'],
+  }
+
+
+
+
+  service { 'httpd':
+    ensure 		=> running,
+    name 		=> ntpd,
+    enable 		=> true,
+    subscribe 	=> [File['ntp_config']]
+  }
 }
