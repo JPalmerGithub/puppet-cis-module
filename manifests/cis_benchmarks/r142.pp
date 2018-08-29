7f23b4b978d4bd61fb198e88a366eab0749991fa
root <root@ip-172-30-0-40.ec2.internal>
2018-08-29 06:26:22 +0000

    fix r142

--- manifests/cis_benchmarks/r142.pp
+++ manifests/cis_benchmarks/r142.pp
@@ -1,13 +1,11 @@
 # Single User Mode Authentication
 class cis::cis_benchmarks::r142 {
 
-  exec { 'single-user-auth':
-    path => [ '/bin' ],
-    command => "sed -i 's/SINGLE=[^ ]*/SINGLE=\\/sbin\\/sulogin/' /etc/sysconfig/init",
-    user => 'root',
-    unless => 'grep ^SINGLE /etc/sysconfig/init | grep sulogin'
+  file_line { 'enable_single_user_auth':
+    line => 'SINGLE=/sbin/sulogin',
+    path => "/etc/sysconfig/init",
+    match => '^SINGLE=',
+    replace => true,
   }
-
-
 
 }
