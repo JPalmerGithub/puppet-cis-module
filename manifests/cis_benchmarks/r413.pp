c201348e70940ab8b1854eec2fd6cc65b8d66db2
root <root@ip-172-30-0-40.ec2.internal>
2018-08-29 03:07:17 +0000

    fix r413

--- manifests/cis_benchmarks/r413.pp
+++ manifests/cis_benchmarks/r413.pp
@@ -1,11 +1,18 @@
 # Audit processes prior to auditd
 class cis::cis_benchmarks::r413 {
 
-  exec { 'enable-auditing':
-    path => '/bin',
-    command => "sed -i '/^\s*kernel/s/$/ audit=1/' /boot/grub/menu.lst",
-    user => 'root',
-    unless => 'grep "^\s*kernel" /boot/grub/menu.lst | grep audit=1'
-  }
+  file_line { 'enable_audit_before_auditd':
+    line => 'GRUB_CMDLINE_LINUX="audit=1 console=tty0 crashkernel=auto console=ttyS0,115200"',
+    path => "/etc/default/grub",
+    match => '^GRUB_CMDLINE_LINUX=',
+    replace => true,
+    ensure   => present,
+    notify => Exec['apply_new_grub_conf'],
+  } 
+
+  exec { 'apply_new_grub_conf':
+    command => '/sbin/grub2-mkconfig > /boot/grub2/grub.cfg',
+    refreshonly =>true,
+  }  
 
 }
