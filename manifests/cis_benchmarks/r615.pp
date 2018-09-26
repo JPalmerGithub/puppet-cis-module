012956b165ad354abab3ead2a0fdc109bf597e10
root <root@ip-172-30-0-126.ec2.internal>
2018-09-26 13:26:14 +0000

    fix r615

--- manifests/cis_benchmarks/r615.pp
+++ manifests/cis_benchmarks/r615.pp
@@ -1,11 +1,8 @@
 # Ensure permissions on /etc/gshadow are configured
 class cis::cis_benchmarks::r615 {
 
-  file { '/etc/gshadow':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0000'
+  exec { '/etc/gshadow_permission':
+    command => '/bin/chmod 0000 /etc/gshadow',
+    onlyif => '/bin/test ! $(/bin/stat -c %a /etc/gshadow) = 0',
   }
-
 }
