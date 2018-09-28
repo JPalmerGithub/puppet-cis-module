eddb06f92e16c068bef4ee7886ade83dd9c692e7
John Alvero <johnhomer@gmail.com>
2018-09-28 17:38:25 +0800

    Update r413.pp

--- manifests/cis_benchmarks/r413.pp
+++ manifests/cis_benchmarks/r413.pp
@@ -1,18 +1,43 @@
 # Audit processes prior to auditd
 class cis::cis_benchmarks::r413 {
 
-  file_line { 'enable_audit_before_auditd':
-    line => 'GRUB_CMDLINE_LINUX="audit=1 console=tty0 crashkernel=auto console=ttyS0,115200"',
-    path => "/etc/default/grub",
-    match => '^GRUB_CMDLINE_LINUX=',
-    replace => true,
-    ensure   => present,
-    notify => Exec['apply_new_grub_conf'],
-  } 
 
-  exec { 'apply_new_grub_conf':
-    command => '/sbin/grub2-mkconfig > /boot/grub2/grub.cfg',
-    refreshonly =>true,
-  }  
+  if $osfamily == 'Redhat' and $operatingsystemmajrelease == '7' {
+    $grub_config_path = "/etc/default/grub"
+  }
+  elsif $osfamily == 'Redhat' and $operatingsystemmajrelease == '6' {
+    $grub_config_path = "/etc/sysconfig/grub" 
+  } else {
+    $grub_config_path = "/etc/sysconfig/grub"
+  }
+
+
+  if $osfamily == 'Redhat' and $operatingsystemmajrelease == '7' {
+
+    file_line { 'enable_audit_before_auditd':
+      line => 'GRUB_CMDLINE_LINUX="audit=1 console=tty0 crashkernel=auto console=ttyS0,115200"',
+      path => "$grub_config_path",
+      match => '^GRUB_CMDLINE_LINUX=',
+      replace => true,
+      ensure   => present,
+      notify => Exec['apply_new_grub_conf'],
+    } 
+
+    exec { 'apply_new_grub_conf':
+      command => '/sbin/grub2-mkconfig > /boot/grub2/grub.cfg',
+      refreshonly =>true,
+    }
+
+  } elsif $osfamily == 'Redhat' and $operatingsystemmajrelease == '6' {
+
+    file_line { 'enable_audit_before_auditd':
+      line => 'GRUB_CMDLINE_LINUX="audit=1 console=tty0 crashkernel=auto console=ttyS0,115200"',
+      path => "$grub_config_path",
+      match => '^GRUB_CMDLINE_LINUX=',
+      replace => true,
+      ensure   => present,
+    } 
+
+  }
 
 }
