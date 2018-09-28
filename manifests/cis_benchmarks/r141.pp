e6372a0987877ac763f558d9f96749a1bee7f38c
John Alvero <johnhomer@gmail.com>
2018-09-28 17:37:18 +0800

    Update r141.pp

--- manifests/cis_benchmarks/r141.pp
+++ manifests/cis_benchmarks/r141.pp
@@ -1,19 +1,23 @@
 # Set Bootloader Permissions
 class cis::cis_benchmarks::r141 {
 
+  Exec {
+    path => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+  }
+
   exec {"change_permission_menu_dot_list":
-    command => '/bin/chmod 0600 /boot/grub/menu.lst',
-    onlyif => '/usr/bin/test ! -L /boot/grub/menu.lst',
+    command => 'chmod 0600 /boot/grub/menu.lst',
+    onlyif => 'test ! -L /boot/grub/menu.lst',
   }
 
   exec { 'grub_cfg_ownership':
-    command => '/bin/chown root:root /boot/grub2/grub.cfg',
-    onlyif => '/bin/test ! $(/bin/stat -c %U%G /boot/grub2/grub.cfg) = rootroot',
+    command => 'chown root:root /boot/grub2/grub.cfg',
+    onlyif => 'test ! $(stat -c %U%G /boot/grub2/grub.cfg) = rootroot',
   }
   
   exec { 'grub_cfg_permission':
-    command => '/bin/chmod 600 /boot/grub2/grub.cfg',
-    onlyif => '/bin/test ! $(/bin/stat -c %a /boot/grub2/grub.cfg) = 600',
+    command => 'chmod 600 /boot/grub2/grub.cfg',
+    onlyif => 'test ! $(stat -c %a /boot/grub2/grub.cfg) = 600',
   }
 
 
