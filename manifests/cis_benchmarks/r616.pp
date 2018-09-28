9936dd811b95d18d5490a1b9d9ca56947c05b7d7
John Alvero <johnhomer@gmail.com>
2018-09-28 17:40:56 +0800

    Update r616.pp

--- manifests/cis_benchmarks/r616.pp
+++ manifests/cis_benchmarks/r616.pp
@@ -1,9 +1,13 @@
 # Ensure permissions on /etc/passwd- are configured
 class cis::cis_benchmarks::r616 {
 
+  Exec {
+    path => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+  }
+
   exec { '/etc/passwd-_permission':
-    command => '/bin/chmod 600 /etc/passwd-',
-    onlyif => '/bin/test ! $(/bin/stat -c %a /etc/passwd-) = 600',
+    command => 'chmod 600 /etc/passwd-',
+    onlyif => 'test ! $(stat -c %a /etc/passwd-) = 600',
     require => File['/etc/security/pwquality.conf'],
   }
 }
