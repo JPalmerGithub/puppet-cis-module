f31f9cf5d0334b4f8591af41a43be201f0fed6a6
root <root@ip-172-30-0-42.ec2.internal>
2018-09-28 02:48:35 +0000

    change permission menu.list only if not symlink

--- manifests/cis_benchmarks/r141.pp
+++ manifests/cis_benchmarks/r141.pp
@@ -3,7 +3,7 @@ class cis::cis_benchmarks::r141 {
 
   exec {"change_permission_menu_dot_list":
     command => '/bin/chmod 0600 /boot/grub/menu.lst',
-    onlyif => '/usr/bin/test -e /boot/grub/menu.lst',
+    onlyif => '/usr/bin/test ! -L /boot/grub/menu.lst',
   }
 
   exec { 'grub_cfg_ownership':
