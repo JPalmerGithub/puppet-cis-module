473ec7f0b648e1f8034d5739e5b1a1f432d982df
John Alvero <johnhomer@gmail.com>
2018-09-28 17:42:44 +0800

    Update r619.pp

--- manifests/cis_benchmarks/r619.pp
+++ manifests/cis_benchmarks/r619.pp
@@ -1,9 +1,13 @@
 # Ensure permissions on /etc/gshadow- are configured
 class cis::cis_benchmarks::r619 {
 
+  Exec {
+    path => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+  }
+  
   exec { '/etc/gshadow-_permission':
-    command => '/bin/chmod 600 /etc/gshadow-',
-    onlyif => '/bin/test ! $(/bin/stat -c %a /etc/gshadow-) = 600',
+    command => 'chmod 600 /etc/gshadow-',
+    onlyif => 'test ! $(/bin/stat -c %a /etc/gshadow-) = 600',
     require => File['/etc/security/pwquality.conf'],
   }
 
