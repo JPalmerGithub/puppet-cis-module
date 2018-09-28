946d52fa4425813d807bf95fd583cc2753303e75
John Alvero <johnhomer@gmail.com>
2018-09-28 17:41:29 +0800

    Update r617.pp

--- manifests/cis_benchmarks/r617.pp
+++ manifests/cis_benchmarks/r617.pp
@@ -1,9 +1,13 @@
 # Ensure permissions on /etc/shadow- are configured
 class cis::cis_benchmarks::r617 {
 
+  Exec {
+    path => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+  }
+
   exec { '/etc/shadow-_permission':
-    command => '/bin/chmod 600 /etc/shadow-',
-    onlyif => '/bin/test ! $(/bin/stat -c %a /etc/shadow-) = 600',
+    command => 'chmod 600 /etc/shadow-',
+    onlyif => 'test ! $(stat -c %a /etc/shadow-) = 600',
     require => File['/etc/security/pwquality.conf'],
   }
 
