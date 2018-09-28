b488232d3c3b252392f59de02960711bd7dbc6d5
John Alvero <johnhomer@gmail.com>
2018-09-28 17:40:21 +0800

    Update r615.pp

--- manifests/cis_benchmarks/r615.pp
+++ manifests/cis_benchmarks/r615.pp
@@ -1,8 +1,12 @@
 # Ensure permissions on /etc/gshadow are configured
 class cis::cis_benchmarks::r615 {
 
+  Exec {
+    path => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+  }
+
   exec { '/etc/gshadow_permission':
-    command => '/bin/chmod 0000 /etc/gshadow',
-    onlyif => '/bin/test ! $(/bin/stat -c %a /etc/gshadow) = 0',
+    command => 'chmod 0000 /etc/gshadow',
+    onlyif => 'test ! $(stat -c %a /etc/gshadow) = 0',
   }
 }
