96b28a730aa23f03b3246299c92b3e83312a4777
John Alvero <johnhomer@gmail.com>
2018-09-28 18:32:46 +0800

    Update r55.pp

--- manifests/cis_benchmarks/r55.pp
+++ manifests/cis_benchmarks/r55.pp
@@ -1,12 +1,21 @@
 # Ensure access to the su command is restricted
 class cis::cis_benchmarks::r55 {
 
+if $osfamily == 'Redhat' and $operatingsystemmajrelease == '7' {
+    $su_config_path = "puppet:///modules/cis/pam/su"
+  }
+  elsif $osfamily == 'Redhat' and $operatingsystemmajrelease == '6' {
+    $su_config_path = "puppet:///modules/cis/pam/su_centos6" 
+  } else {
+    $su_config_path = "puppet:///modules/cis/pam/su"
+  }
+
   file { '/etc/pam.d/su':
     ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0644',
-    source => 'puppet:///modules/cis/user_accounts/su'
+    source => "$su_config_path",
   } 
 
 }
